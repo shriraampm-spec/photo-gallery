656d4c182af77f379186881ce37fecbcb81b4651
shriraampm-spec <shriraampm@gmail.com>
2026-04-04 18:43:32 +1000

    Update axiosConfig.jsx

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5001',
+  baseURL: process.env.REACT_APP_API_URL || 'http://3.24.168.16:5001',
 });
 
 export default axiosInstance;
