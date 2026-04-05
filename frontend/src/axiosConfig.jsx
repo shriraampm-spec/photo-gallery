02914736752493c0b97d66c8a3d4ca9cc3d32da7
shriraampm-spec <shriraampm@gmail.com>
2026-04-05 14:25:31 +1000

    ci: resolved pm2 frontend issue

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+const hostname = window.location.hostname;
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://3.24.168.16:5001',
+  baseURL: process.env.REACT_APP_API_URL || `http://${hostname}:5001`,
 });
 
 export default axiosInstance;
